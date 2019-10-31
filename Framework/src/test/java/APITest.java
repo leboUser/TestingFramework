082f5c1cb6a03f6b61c3dc7ee678461bfdd0a491
leboUser <lebomokaila22@gmail.com>
2019-10-31 12:09:27 +0200

    Added path location

--- Framework/src/test/java/APITest.java
+++ Framework/src/test/java/APITest.java
@@ -1,9 +1,14 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class APITest {
 
+    String Path ="";
+
+    @Parameters({"excelPath"})
     @Test
-    private void test(){
-        System.out.println("HEllo world !!");
+    private void test(String excelPath){
+        System.out.println(excelPath);
     }
 }
